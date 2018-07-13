d5795e9f1fd88ba78429e162a8c93d56b8b49b74
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-07-13 13:04:12 +0430

    Add files via upload

---
+++ ad.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:47:12 11/21/2017 
+// Design Name: 
+// Module Name:    ad 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ad(ci,a0,a1,b0,b1
+    );
+
+
+endmodule
