ac5ce520df72bbba23c89ac871935a373dd3f7af
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-07-28 16:01:40 +0430

    Add files via upload

---
+++ midfull.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    17:16:04 11/28/2017 
+-- Design Name: 
+-- Module Name:    midfull - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity midfull is
+end midfull;
+
+architecture Behavioral of midfull is
+
+begin
+
+
+end Behavioral;
+
