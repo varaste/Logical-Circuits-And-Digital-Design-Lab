de509b9bf23ca02f3478153b35746633e00a8d99
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-08-22 20:25:42 +0430

    Delete _primary.vhd

--- my-project/joftesh/aaa/gray/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity gray is
-    port(
-        a               : in     vl_logic_vector(3 downto 0);
-        y               : out    vl_logic_vector(3 downto 0)
-    );
-end gray;
